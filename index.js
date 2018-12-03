cfc26690e6bf4126f8173bf34b7a787dc4490b21
Julie Persinger <julie.persinger@willowtreeapps.com>
2018-12-03 11:20:35 -0500

    Add port

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 3001;
+
 app.get("/recipes", (req, res) => {
   res.json({ recipes: "recipe" });
 });
 
-app.listen();
+app.listen(port);
