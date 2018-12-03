5dded4a0a4d921e49077cde6bbf72207a2d92c40
Julie Persinger <julie.persinger@willowtreeapps.com>
2018-12-03 12:00:39 -0500

    Remove setup function

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-import { getRecipes, setup } from "./services/redis";
+import { getRecipes } from "./services/redis";
 
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3001;
-console.log("setup");
-setup();
 
 app.get("/recipes", (req, res) => {
   res.json({ recipes: getRecipes() });
--- services/redis/index.ts
+++ services/redis/index.ts
@@ -2,23 +2,26 @@ import redis, { ClientOpts } from "redis";
 import url from "url";
 import { RECIPES } from "./constants";
 
-let client: redis.RedisClient;
-
-export const setup = () => {
+const getClient = (): redis.RedisClient => {
   if (process.env.REDISTOGO_URL) {
     const rtg = url.parse(process.env.REDISTOGO_URL);
     console.log("rtg", rtg, rtg.hostname as ClientOpts);
-    client = redis.createClient(rtg.port || "", rtg.hostname as ClientOpts);
+    const client = redis.createClient(
+      rtg.port || "",
+      rtg.hostname as ClientOpts
+    );
     console.log("client", client);
 
     const auth = rtg.auth || "";
     console.log("auth", auth);
     client.auth(auth.split(":")[1]);
     console.log("client again", client);
+    return client;
   } else {
     console.log("creating new client");
-    client = redis.createClient();
+    const client = redis.createClient();
     console.log("new client", client);
+    return client;
   }
 
   // setInterval(() => {
@@ -28,20 +31,21 @@ export const setup = () => {
 };
 
 export const getRecipes = (): any => {
+  const client = getClient();
   return client.get(RECIPES);
 };
 
-const sendLocalData = () => {
-  console.log("sending");
-  client.hset(RECIPES, "oatmeal", "piece 1", redis.print);
-};
+// const sendLocalData = () => {
+//   console.log("sending");
+//   client.hset(RECIPES, "oatmeal", "piece 1", redis.print);
+// };
 
-const pullServerData = () => {
-  console.log("pulling");
-  client.hkeys(RECIPES, (err, replies) => {
-    console.log(replies.length, " replies:");
-    replies.forEach((reply, i) => {
-      console.log("        ", i, ": ", reply);
-    });
-  });
-};
+// const pullServerData = () => {
+//   console.log("pulling");
+//   client.hkeys(RECIPES, (err, replies) => {
+//     console.log(replies.length, " replies:");
+//     replies.forEach((reply, i) => {
+//       console.log("        ", i, ": ", reply);
+//     });
+//   });
+// };
